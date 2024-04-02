68f08aaed9f7cea3427c87453c7952df951e7111
Aqsa Shaikh <160129099+Aqsamajeed00@users.noreply.github.com>
2024-04-02 21:50:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let myFavouriteCar = "Civic";
+let myFavouritecolor = "Black";
+console.log(myFavouriteCar == "Civic" && myFavouritecolor == "White");
+// let myfavouriteFood: string = "Biryani";
+// let myfavouriteDrink: string = "Pepsi";
+// console.log(myfavouriteFood == "Biryani" || myfavouriteDrink == "Coca Cola");
---
+++ main.ts
@@ -0,0 +1,33 @@
+// LOGICAL OPERATORS!!!!
+let myFavouriteCar: string = "Civic";
+let myFavouritecolor: string = "Black";
+console.log(myFavouriteCar == "Civic" && myFavouritecolor == "White");
+
+let myfavouriteFood: string = "Biryani";
+let myfavouriteDrink: string = "Pepsi";
+console.log(myfavouriteFood == "Biryani" || myfavouriteDrink == "Coca Cola");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
